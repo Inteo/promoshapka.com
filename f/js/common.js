0d7e24d619e3801f816ec278f1ed7e24cc8595ad
BerTepesh <Ber.Tepesh@gmail.com>
2015-06-18 10:43:07 +0400

    Слайдер

--- f/js/common.js
+++ f/js/common.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
-	
+
 });
 $(function(){
 	$(".ham-btn").click(function(){
 		$(".main-menu_ham").toggleClass("active");
+	});
+	$(".search-trigger").click(function(){
+		$(".catalog-search").toggleClass("active");
+		if($(".catalog-search").hasClass("active")) 
+		{
+			$(".catalog-search-close").show();
+			$(".catalog-menu ul").hide();
+		}
+		else 
+		{
+			$(".catalog-search-close").hide();
+			$(".catalog-menu ul").show();
+		}
 	});
 });
